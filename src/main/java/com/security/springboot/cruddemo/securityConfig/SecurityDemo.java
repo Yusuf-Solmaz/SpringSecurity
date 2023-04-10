ab9bbac98038ed9a8a73623c3785f6dd21371824
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-04-11 00:02:43 +0300

    Authentication

--- src/main/java/com/security/springboot/cruddemo/securityConfig/SecurityDemo.java
+++ src/main/java/com/security/springboot/cruddemo/securityConfig/SecurityDemo.java
@@ -2,14 +2,45 @@ package com.security.springboot.cruddemo.securityConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityDemo {
 
+    @Bean
+    public UserDetailsManager userDetailsManager (DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+
         @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+            http.authorizeHttpRequests( configurer ->
+                    configurer
+                            .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                            .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                            .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                            .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+                            .requestMatchers(HttpMethod.DELETE,"/api/employees").hasRole("ADMIN")
+            );
+            http.httpBasic();
+            http.csrf().disable();
+
+            return http.build();
+        }
+
+        /*
+
+         @Bean
         public InMemoryUserDetailsManager userDetailsManager(){
 
             UserDetails yusuf = User.builder()
@@ -25,13 +56,15 @@ public class SecurityDemo {
                     .build();
 
             UserDetails ahmet = User.builder()
-                    .username("erkam")
-                    .password("{noop}erkam123")
+                    .username("ahmet")
+                    .password("{noop}ahmet123")
                     .roles("EMPLOYEE,ADMIN,MANAGER")
                     .build();
 
             return new InMemoryUserDetailsManager(yusuf,erkam,ahmet);
         }
+        */
+
 
     }
 
